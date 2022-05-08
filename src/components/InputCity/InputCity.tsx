26752a55915250e910c4b903635a7ebbfab341f7
Быков Роман <bykov@softlab.ru>
2022-05-08 10:23:55 +0300

    feat(getWeatherData): запрос данных о погоде
    Организован запрос данных о погоде в выбранном городе

--- src/components/InputCity/InputCity.tsx
+++ src/components/InputCity/InputCity.tsx
@@ -1,8 +1,50 @@
+import { useRef, useState } from "react";
+import { WEATHER_FORECAST_BASIC_API_URL } from "../../config/config";
 import { InputCityStyled } from "./InputCity.styles"
 
 const InputCity = () => {
+  const [cityName, setCityName] = useState('');
+  const cityNameInputValue = useRef<HTMLInputElement>(null);
+
+  /**
+   * Обработчик нажатия клавиш
+   * @param e событие нажатия клавиши
+   */
+  const keyPressHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === 'Enter' && cityName) {
+      getWeatherData(cityName);
+    }
+  }
+
+  /**
+   * Обработчик ввода в форму
+   * @param e событие формы
+   */
+  const inputHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    setCityName(e.currentTarget.value);
+  }
+  
+  /**
+   * Запрашивает данные о погоде в городе с последующей записью результата в хранилище 
+   * @param name - название города
+   */
+  const getWeatherData = async (name: string): Promise<void> => {
+    try {
+      const response = await fetch(WEATHER_FORECAST_BASIC_API_URL + name + '&days=3')
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <InputCityStyled placeholder='Выбрать город'/>
+    <InputCityStyled 
+      placeholder='Выбрать город' 
+      onKeyDown={keyPressHandler} 
+      ref={cityNameInputValue} 
+      onChange={inputHandler}
+    />
   )
 }
 
---
+++ src/config/config.ts
@@ -0,0 +1,6 @@
+/**
+ * Ключ для подключения к weather API 
+ */
+const WEATHER_API_KEY: string = 'd1bf1f91bf394ff0a1251938220605';
+
+export const WEATHER_FORECAST_BASIC_API_URL: string = `http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=`
