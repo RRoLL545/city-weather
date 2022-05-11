bc01a575daf038047be3eca353c8826cd1962216
Быков Роман <bykov@softlab.ru>
2022-05-11 08:23:56 +0300

    Развёртывание приложение

--- src/hooks/redux.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../stateManagement';
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
